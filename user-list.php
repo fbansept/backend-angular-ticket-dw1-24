9e69b6dec0b4e4e41223201ebd5de7e9ab96e11c
fbansept <bansept.franck@gmail.com>
2024-06-03 11:57:45 +0200

    gestion de role

--- user-list.php
+++ user-list.php
@@ -1,6 +1,15 @@
 <?php
 
 include 'header-init.php';
+include 'jwt-helper.php';
+
+$user = extractJwtBody();
+
+if ($user->role != "Administrateur") {
+    http_response_code(403);
+    echo '{"message" : "Vous n\avez pas les droits nécessaires"}';
+    exit();
+}
 
 $request = $connexion->query("  SELECT u.id, email, lastname, firstname, name AS role_name  
                                 FROM user AS u 
